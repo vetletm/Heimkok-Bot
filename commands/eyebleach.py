a03060027189ade5ecdc3c21bd71339724b9b391
vetletm <vetlemoen@gmail.com>
2019-12-22 16:49:20 +0100

    add skeleton for listing available subreddits

--- commands/eyebleach.py
+++ commands/eyebleach.py
@@ -12,6 +12,7 @@ class Eyebleacher:
             - Search all files starting with subreddit for name as a suffix
         - Can either use praw for direct interaction with Reddit API, or:
         - Use requests to manually scrape the json data from a subreddit
+        - upgrade to pycharm when possible....
 
     If using requests the flow will be:
         - fetch subreddit from self.subreddits
@@ -20,6 +21,9 @@ class Eyebleacher:
                 if date is not today, refresh content
         - pick top post from subreddit content
         - return top post
+
+    Using requests might result in response 429 from Reddit, if this turns out to be
+    a persistent issue, reconsider using Praw.
     """
     def __init__(self):
         self.endpoint = 'http://www.reddit.com/r/'
@@ -27,6 +31,13 @@ class Eyebleacher:
 
         with open('commands/data/subreddits.json', 'r') as file:
             self.subreddits = json.loads(file.read())
+
+    def list_subreddits(self) -> str:
+        """
+        Will return a list of all available subreddits
+        :return: Str of a joined list of subreddits as found in "subreddits.json"
+        """
+        pass
 
     def fetch_random(self) -> str:
         """
@@ -52,6 +63,7 @@ class Eyebleacher:
 
     def _update_state(self, subreddit: str) -> bool:
         """
-        Stores the state of the subreddit data to avoid too many calls to reddit
+        Stores the state of the subreddit data to avoid too many calls to reddit,
+        if the state is not stored create new file with name "subreddit_name.json"
         """
         pass
