31f6580230c3fc88877c79eced8756494dd827df
Vetle Moen <vetletm@users.noreply.github.com>
2019-12-23 12:34:53 +0100

    Add logging
    * update gitignore
    
    * Add logging to heimkok.py

--- heimkok.py
+++ heimkok.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 
 import os
+import logging
 
 from discord.ext import commands
 
 from bot_commands import BotCommands
 
+# Set up logging
+logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Set up bot
 bot = commands.Bot(command_prefix='!',
                    description='Heimkok is a simple bot for simple stuff')
 
-if 'BOT_TOKEN' in os.environ:
+# Attempt to get the BOT_TOKEN
+BOT_TOKEN = None
+try:
     BOT_TOKEN = os.environ['BOT_TOKEN']
-else:
-    print('BOT_TOKEN must be passed as an environment variable')
+    logger.info('Sucessfully imported BOT_TOKEN')
+except Exception as error:
+    logger.critical('Could not import BOT_TOKEN, error: %s', error)
+    exit(1)
+
+if not BOT_TOKEN:
+    logger.critical('Fetching BOT_TOKEN failed without exception, exiting')
     exit(1)
 
 
@@ -29,24 +42,36 @@ async def joke(ctx):
 async def joke_norris(ctx):
     """ A kick-ass Chuck Norris joke!
         """
-    response = command.get_joke('norris')
-    await ctx.send(response)
+    try:
+        response = command.get_joke('norris')
+        await ctx.send(response)
+        logger.info('Fetched and returned joke of type norris')
+    except Exception as error:
+        logger.warning('Could not get joke of type norris, error: %s', error)
 
 
 @joke.command(name='programming')
 async def joke_programming(ctx):
     """ Nerdy jokes
         """
-    response = command.get_joke('programming')
-    await ctx.send(response)
+    try:
+        response = command.get_joke('programming')
+        await ctx.send(response)
+        logger.info('Fetched and returned a joke of type programming')
+    except Exception as error:
+        logger.warning('Could not get joke of type programming, error: %s', error)
 
 
 @joke.command(name='random')
 async def joke_random(ctx):
     """ Random, funny jokes
     """
-    response = command.get_joke('random')
-    await ctx.send(response)
+    try:
+        response = command.get_joke('random')
+        await ctx.send(response)
+        logger.info('Fetched and returned a random joke')
+    except Exception as error:
+        logger.warning('Could not get joke of random type, error: %s', error)
 
 
 @commands.cooldown(rate=1, per=1, type=commands.BucketType.default)
@@ -55,17 +80,25 @@ async def weather(ctx, req):
     """ Gives you some information based on your desired city.
     Usage: !weather Oslo (keep it unambiguous)
         """
-    city_request = ''.join(req)
-    response = command.get_weather(city_request)
-    await ctx.send(response)
+    try:
+        city_request = ''.join(req)
+        response = command.get_weather(city_request)
+        await ctx.send(response)
+        logger.info('Fetched and returned the weather for City %s', city_request)
+    except Exception as error:
+        logger.warning('Could not get the weather for city: %s, error: %s', city_request, error)
 
 
 @bot.command()
 async def catfact(ctx):
     """ Will provide you with a neat cat fact!
         """
-    response = command.get_catfact()
-    await ctx.send(response)
+    try:
+        response = command.get_catfact()
+        await ctx.send(response)
+        logger.info('Fetched and returned a catfact')
+    except Exception as error:
+        logger.warning('Could not fetch catfact, error: %s', error)
 
 
 @bot.group(invoke_without_command=True)
@@ -75,56 +108,85 @@ async def cuteanimal(ctx):
         If the animal you want to see is not in the list, it will return a random picture instead.
         No specified animal will result in a random picture.
     """
-    response = command.get_animal()
-    await ctx.send(response)
+    try:
+        response = command.get_animal()
+        await ctx.send(response)
+        logger.info('Fetched and returned a picture of a cute animal')
+    except Exception as error:
+        logger.warning('Could not fetch a picture of a cute animal, error: %s', error)
 
 
 @cuteanimal.command(name='cat')
 async def cuteanimal_cat(ctx):
     """ returns a cute cat
     """
-    response = command.get_animal('cat')
-    await ctx.send(response)
+    try:
+        response = command.get_animal('cat')
+        await ctx.send(response)
+        logger.info('Fetched and returned a picture of a cat')
+    except Exception as error:
+        logger.warning('Could not fetch cat picture, error: %s', error)
 
 
 @cuteanimal.command(name='shibe')
 async def cuteanimal_shibe(ctx):
     """ returns a cute shiba inu
     """
-    response = command.get_animal('shibe')
-    await ctx.send(response)
+    try:
+        response = command.get_animal('shibe')
+        await ctx.send(response)
+        logger.info('Fetched and returned a picture of a shiba inu')
+    except Exception as error:
+        logger.warning('Could not fetch shiba inu picture, error: %s', error)
 
 
 @cuteanimal.command(name='fox')
 async def cuteanimal_fox(ctx):
     """ returns a cute fox
     """
-    response = command.get_animal('fox')
-    await ctx.send(response)
+    try:
+        response = command.get_animal('fox')
+        await ctx.send(response)
+        logger.info('Fetched and returned a picture of a fox')
+    except Exception as error:
+        logger.warning('Could not fetch picture of a fox, error: %s', error)
 
 
 @cuteanimal.command(name='dog')
 async def cuteanimal_dog(ctx):
     """ returns a cute dog
     """
-    response = command.get_animal('dog')
-    await ctx.send(response)
+    try:
+        response = command.get_animal('dog')
+        await ctx.send(response)
+        logger.info('Fetched and returned a picture of a dog')
+    except Exception as error:
+        logger.warning('Could not fetch a picture of a dog, error: %s', error)
 
 
 @cuteanimal.command(name='dog2')
 async def cuteanimal_dog(ctx):
     """ more dogs!
     """
-    response = command.get_animal('dog2')
-    await ctx.send(response)
+    try:
+        response = command.get_animal('dog2')
+        await ctx.send(response)
+        logger.info('Fetched and returned a picture of a dog2')
+    except Exception as error:
+        logger.warning('Could not fetch a picture of a dog2, error: %s', error)
 
 
 @bot.event
 async def on_ready():
     name = bot.user.name
-    print('Logged in as %s and ready to mingle!' % name)
+    logger.info('Logged in as %s and ready to mingle!', name)
 
 
 command = BotCommands()
 
-bot.run(BOT_TOKEN)
+try:
+    bot.run(BOT_TOKEN)
+    logger.info('Started bot!')
+except Exception as error:
+    logger.critical('Failed to start bot, error: %s', error)
+    exit(2)
